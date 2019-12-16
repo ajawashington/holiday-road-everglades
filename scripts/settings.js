ef8e9aaf0b6caf24c70640022b5ba56d766a9202
Case Scally <51033967+casescally@users.noreply.github.com>
2019-12-16 13:56:11 -0600

    Remove settings file

--- scripts/settings.js
+++
@@ -1,5 +0,0 @@
-export default {
-    npsKey: "e7nUtP2jRRovkoLURmKzkGQAqyIbCktiHsgdj9Yx",
-    weatherKey: "d4c5e7458e49808cd9cd51417901789a",
-    graphhopperKey: "1f309503-e7c1-460e-b134-22847b526b7d"
-}
