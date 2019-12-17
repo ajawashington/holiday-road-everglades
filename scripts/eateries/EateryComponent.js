8d26b31e19d9c73ae279be136b5551f4dd680fe2
Spencer Truett <srtruett1993@gmail.com>
2019-12-17 13:18:03 -0600

    added eatery fetch

---
+++ scripts/eateries/EateryComponent.js
@@ -0,0 +1,22 @@
+const EateryComponent = (eateries) => {
+  return `
+  <section class="content__eateries highlight">
+    <dialog class="content__eateries dialog eatery_dialog">
+      <ul>
+        <span class="thumbnail">
+          <li>${eateries.businessName}</li>
+          <span>
+            <image class="info-pic-size" src="" alt="" />
+            <br>
+            <h4>${eateries.businessName}</h4>
+            <h5>${eateries.city}, ${eateries.state}</h5>
+            <p>${eateries.description}</p>
+          </span>
+        </span>
+
+      </dialog>
+  </section>
+  `
+}
+
+export default EateryComponent
--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,11 @@
 import { getParks } from "./parks/ParkProvider.js";
 import { parkSelect } from "./parks/parkSelect.js";
+import { getEateries } from "./eateries/EateryProvider.js";
+import EateryList from "./eateries/EateryList.js";
+
 
 
 getParks().then(
 () => parkSelect())
+getEateries()
+EateryList()
