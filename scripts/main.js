24901abdbb5b7fed84de27cd5a2438dc7b55c2c5
ajawashington13 <aja.washington13@gmail.com>
2019-12-18 10:25:24 -0600

    spacesRemoved

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,6 @@
-import { regionSelect } from "./regions /regionSelect.js";
-import { useRegions, getRegions, getParksbyRegion } from "./regions /regionsProvider.js";
-import { selectedRegion } from "./regions /regionComponent.js";
+import { regionSelect } from "./regions/regionSelect.js";
+import { useRegions, getRegions, getParksbyRegion } from "./regions/regionsProvider.js";
+import { selectedRegion } from "./regions/regionComponent.js";
 import { getParks, useParks } from "./parks/ParkProvider.js"
 import ParkListComponent from "./parks/parkList.js"
 
