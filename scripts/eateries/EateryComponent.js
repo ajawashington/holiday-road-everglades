0a83b395b25953175723ddafee799a8604d626c5
Spencer Truett <srtruett1993@gmail.com>
2019-12-18 09:33:09 -0600

    new files added; minor js tweaks

--- scripts/eateries/EateryComponent.js
+++ scripts/eateries/EateryComponent.js
@@ -2,18 +2,20 @@ const EateryComponent = (eateries) => {
   return `
   <section class="content__eateries highlight">
     <dialog class="content__eateries dialog eatery_dialog">
-      <ul>
-        <span class="thumbnail">
-          <li>${eateries.businessName}</li>
-          <span>
-            <image class="info-pic-size" src="" alt="" />
-            <br>
-            <h4>${eateries.businessName}</h4>
-            <h5>${eateries.city}, ${eateries.state}</h5>
-            <p>${eateries.description}</p>
+      <section class="eatery__content">
+        <ul>
+          <span class="thumbnail">
+            <li>${eateries.businessName}</li>
+            <span>
+              <image class="info-pic-size" src="" alt="" />
+              <br>
+              <h4>${eateries.businessName}</h4>
+              <h5>${eateries.city}, ${eateries.state}</h5>
+              <p>${eateries.description}</p>
+            </span>
           </span>
-        </span>
-
+        </ul>
+      </section>
       </dialog>
   </section>
   `
--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,10 @@
 import { getParks, useParks } from "./parks/ParkProvider.js"
 import ParkListComponent from "./parks/parkList.js"
+import { getEateries } from "./eateries/EateryProvider.js"
+import EateryList from "./eateries/EateryList.js"
 
 
 getParks().then(
 () => ParkListComponent())
+getEateries()
+EateryList()
