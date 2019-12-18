b89eb72a570f7dd05819a22e2afa07a24cfb0b27
Case Scally <casescally@gmail.com>
2019-12-18 09:35:21 -0600

    added park images

--- scripts/parks/park.js
+++ scripts/parks/park.js
@@ -1,8 +1,21 @@
 const parkComponent = (park) => {
-    return `<div class="park">
+
+    let parkName = ${park.fullName}.split(" ").join("-")
+
+    return `<div class="${parkName}">
     <div>
         ${park.fullName}
     </div>
+            <button> 
+                <dialog>
+                    <h3>Attractions</h3>
+                        <ul>
+                            <li> ${bizzarie}
+                    <h3>Eatery</h3>
+                        <ul>
+                            <li> ${eatery.businessName}
+            </button>
+        </dialog>
     </div>`
 }
 
