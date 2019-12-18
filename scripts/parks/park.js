17d6a6de9375ccfae7497780b0af677ed74126a6
Case Scally <casescally@gmail.com>
2019-12-18 10:41:36 -0600

    added button to park

--- scripts/parks/park.js
+++ scripts/parks/park.js
@@ -1,21 +1,30 @@
 const parkComponent = (park) => {
     return `
     <section>
-    <div class="park">
+        <div class="park">
 
-    <header class="parkHeader">
-     ${park.fullName}
-    </header>
+            <header class="parkHeader">
+            ${park.fullName}
+            </header>
 
-    <div> 
-   State: ${park.states}
-    </div>
-    <div>
-    Description: ${park.description}
-    </div>
+        <div> 
+            State: ${park.states}
+        </div>
+        <div>
+            Description: ${park.description}
+        </div>
+        <button>
+            <dialog>
+                <h3>Attractions</h3>
+                    <ul>
+                        <li> ${bizzare}
+                <h3>Eatery</h3>
+                    <ul>
+                        <li> ${eatery.businessName}
+        </button>
+            </dialog>
     </section>
     `
-
 }
 
 export default parkComponent
