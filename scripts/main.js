10fa9bcb5208f006ac9afe050690445c98434011
Spencer Truett <srtruett1993@gmail.com>
2019-12-18 09:45:25 -0600

    accepting aja's changes

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,12 @@
+import { regionSelect } from "./regions /regionSelect.js";
+import { useParks, getParks, getParksbyRegion } from "./regions /regionsProvider.js";
+import { selectedRegion } from "./regions /regionComponent.js";
+
+getParks().then(selectedRegion)
+useParks()
+regionSelect()
+getParksbyRegion()
+selectedRegion ()
 import { getParks, useParks } from "./parks/ParkProvider.js"
 import ParkListComponent from "./parks/parkList.js"
 
