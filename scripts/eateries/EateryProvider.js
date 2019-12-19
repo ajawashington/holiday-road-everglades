d6b706a5b9c1a7ef4f18287a5bc33b7c6718268c
ajawashington13 <aja.washington13@gmail.com>
2019-12-19 14:56:59 -0600

    notbroken

---
+++ scripts/eateries/EateryProvider.js
@@ -0,0 +1,25 @@
+// console.log("****Eateries Provider module code****")
+
+let eateries = []
+
+export const useEateries = () => {
+    return eateries
+}
+
+export const getEateries = () => {
+  
+//   console.log("****I am going to get the eateries data****")
+
+    return fetch("http://holidayroad.nss.team/eateries")
+        .then(response => response.json())
+        .then(
+
+            parsedEateries => {
+                // console.table(parsedEateries)
+
+                // console.log("****I have the eateries data****")
+                eateries = parsedEateries.slice()
+
+            }
+        )
+}
